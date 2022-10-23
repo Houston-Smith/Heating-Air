23af857a42e2a6e7ab475d81d1e25c121b2cc0a8
Houston-Smith <houstonbsmith@gmail.com>
2022-10-23 18:07:49 -0500

    Service Call Model

---
+++ Claud/Models/ServiceCall.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Claud.Models
+{
+    public class ServiceCall
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(50)]
+        public string LocationName { get; set; }
+
+
+        [Required]
+        [MaxLength(225)]
+        public string LocationAddress { get; set; }
+
+
+        public DateTime DateScheduled { get; set; }
+
+
+        public DateTime DateService { get; set; }
+
+
+        [Required]
+        [MaxLength(225)]
+        public string Notes { get; set; }
+
+
+        public int UserProfileId { get; set; }
+
+
+    }
+}
--- SQL/02_H&A_Seed_Data.sql.sql
+++ SQL/02_H&A_Seed_Data.sql.sql
@@ -1,19 +1,18 @@
-USE [GravyTrain]
+USE [H&A]
 GO
 
 SET IDENTITY_INSERT [UserProfile] ON
 INSERT INTO [UserProfile] (
-	[Id], [FirebaseUserId], [Username], [FirstName], [LastName], [Email], [CreateDate])
-VALUES (1, 'umxJ5PnWoiZBFTrNolYC97m749p1', 'HSmith', 'Houston', 'Smith', 'H@Smith.com', SYSDATETIME()),
-(2, 'ffhCN0kr3GUv2fWcBdoocbvpj8B2', 'CCarter', 'Chapel', 'Carter', 'C@Carter.com', SYSDATETIME()),
-(3, 'Tq6VSx2DrXZYz6AaapiJWiTdu4f2', 'WMoody', 'Walter', 'Moody', 'W@Moddy.com', SYSDATETIME());
+	[Id], [FirebaseUserId], [Username], [FirstName], [LastName], [Email], [ServiceDate], [CreateDate])
+VALUES (1, 'yM5InvGlC0ehCByb42aZKrBSUIC3', 'Hsmith', 'Houston', 'Smith', 'houstonbsmith@gmail.com', SYSDATETIME(), SYSDATETIME()),
+(2, 'QXjgESoFYog88byQHKrDwLYJXyh2', 'CCarter', 'Chapel', 'Carter', 'chapelcarter@gmail.com', SYSDATETIME(), SYSDATETIME());
 SET IDENTITY_INSERT [UserProfile] OFF
 
-SET IDENTITY_INSERT [Review] ON
-INSERT INTO [Review] ([Id], [LocationName], [LocationAddress], [DateReviewed], [ButteryScore], [FlakeyScore], [GravyScore], [FlavorScore], [DeliveryScore], [AverageScore], [Notes], [GravyType], [UserProfileId])
-VALUES (1, 'Nashville Biscuit House', 'Filler', SYSDATETIME(), 8, 6, 7, 7, 8, 7, 'No Notes', 'White', 1),
-(2, 'Nashville Biscuit House', 'Filler', SYSDATETIME(), 9, 6, 8, 9, 8, 8, 'No Notes', 'White', 2);
-SET IDENTITY_INSERT [Review] OFF
+SET IDENTITY_INSERT [ServiceCall] ON
+INSERT INTO [ServiceCall] ([Id], [LocationName], [LocationAddress], [DateScheduled], [DateService], [Notes], [UserProfileId])
+VALUES (1, 'Filler 1', 'Filler 1', SYSDATETIME(), SYSDATETIME(), 'No Notes', 1),
+(2, 'Filler 2', 'Filler 2', SYSDATETIME(), SYSDATETIME(), 'No Notes', 2);
+SET IDENTITY_INSERT [ServiceCall] OFF
 
 
 
