4e675f7d11cb4432f4ae8a8e55ae391675904772
Houston-Smith <houstonbsmith@gmail.com>
2022-10-23 18:41:12 -0500

    ServiceCall Repository

--- Claud/Repositories/UserProfileRepository.cs
+++ Claud/Repositories/UserProfileRepository.cs
@@ -168,21 +168,5 @@ namespace Claud.Repositories
         }
 
 
-
-
-        /*
-        public UserProfile GetByFirebaseUserId(string firebaseUserId)
-        {
-            return _context.UserProfile
-                       .Include(up => up.UserType) 
-                       .FirstOrDefault(up => up.FirebaseUserId == firebaseUserId);
-        }
-
-        public void Add(UserProfile userProfile)
-        {
-            _context.Add(userProfile);
-            _context.SaveChanges();
-        }
-        */
     }
 }
